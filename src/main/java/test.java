b11c00d4b4da11767d9342a5493495ca2f2c83b2
jie <jie@qq.com>
2021-05-30 23:46:11 +0800

    add test4

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("cccccc");
-        System.out.println("push");
+
     }
 }
